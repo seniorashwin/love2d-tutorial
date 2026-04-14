0bf43b750ce881314dc026628d88fb015415c24c
seniorashwin <seniorashwin@users.noreply.github.com>
2026-04-14 17:25:16 +0530

    added player and background sprite

--- main.lua
+++ main.lua
@@ -2,8 +2,11 @@ function love.load()
     player = {}
     player.x = 400
     player.y = 200
-
     player.speed = 100
+    player.sprite = love.graphics.newImage("sprites/parrot.png")
+
+    background = {}
+    background.sprite = love.graphics.newImage("sprites/background.png")
 end
 
 function love.update(dt)
@@ -25,5 +28,6 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, 10)
+    love.graphics.draw(background.sprite, 0, 0)
+    love.graphics.draw(player.sprite, player.x, player.y)
 end
